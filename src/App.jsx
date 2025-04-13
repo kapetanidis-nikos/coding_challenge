359da5a37bc24f2cd89eb0af011eff6981e6f447
Nikos Kapetanidis <kapetanidis@synelixis.com>
2025-04-13 21:19:37 +0300

    Update background styling

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,13 @@ function App() {
 
   return (
     <div
-      className="flex pt-10 pb-10 min-h-svh w-full bg-gradient-to-br from-blue-900
-       via-sky-900 to-indigo-900 items-center justify-center 
+      className="flex pt-10 pb-10 min-h-svh w-full bg-gradient-to-br from-blue-50
+       via-sky-100 to-blue-100 items-center justify-center 
        relative overflow-hidden p-6 md:p-10"
     >
-      <FloatingShape color="bg-sky-500" size="w-48 h-48" top="70%" left="80%" delay={5} />
-      <FloatingShape color="bg-indigo-400" size="w-32 h-32" top="40%" left="-10%" delay={2} />
+      <FloatingShape color="bg-blue-300" size="w-64 h-64" top="-5%" left="-5%" delay={0} />
+      <FloatingShape color="bg-sky-300" size="w-48 h-48" top="70%" left="80%" delay={5} />
+      <FloatingShape color="bg-blue-300" size="w-32 h-32" top="40%" left="-10%" delay={2} />
 
       <div className="w-3/5 gap-12 flex flex-col">
         <Card title={'Popular Repositories'}>
